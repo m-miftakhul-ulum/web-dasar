b10dd61fb8ce24d27d3f4d6e1bf2c4e90375d932
miftakhul-dev <201240000992@unisnu.ac.id>
2023-03-16 11:49:46 +0700

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,20 @@
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === 'block') {
+                openDropdown.style.display = 'none';
+            }
+        }
+    }
+}
+
+
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
